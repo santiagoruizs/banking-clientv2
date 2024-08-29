1a5f48a6a0cfde6f909158b88baf1f14dbdaaa4e
Santiago Ruiz <sruizs@Santiagos-Laptop.local>
2024-08-29 21:05:04 +0200

    Fixes confirm password fucntion

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -11,38 +11,50 @@ import { Input } from "@/components/ui/input";
 import { Link } from "react-router-dom";
 import { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useToast } from "@/components/ui/use-toast";
+
 
 
 const Signup = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
   const [email, setEmail] = useState('')
   const navigate = useNavigate()
+  const { toast } = useToast()
 
   const handleSignup = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    
-    const payload = {username, email, password}
-    console.log("Signing up with:", {payload});
+    if (password === confirmPassword){
+      const payload = {username, email, password}
+      console.log("Signing up with:", {payload});
 
-    try {
-      const response = await fetch(import.meta.env.VITE_API_URL+'/auth/signup',
-        {
-          method : 'POST',
-          headers : {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(payload)
-        })
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        console.log('Response data:', data);
-        navigate('/login')
-    } catch (error) {
-      console.error('Error:', error);
+      try {
+        const response = await fetch(import.meta.env.VITE_API_URL+'/auth/signup',
+          {
+            method : 'POST',
+            headers : {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(payload)
+          })
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          const data = await response.json();
+          console.log('Response data:', data);
+          navigate('/login')
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    }else{
+      toast({
+        variant: "destructive",
+        title: "Sign In",
+        description: "Passwords do not match",
+      })
     }
+    
   }
   return (
     <div className="flex-auto flex items-center justify-center">
@@ -64,7 +76,7 @@ const Signup = () => {
               <Input id="password" placeholder="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
             </div>
             <div className="flex flex-col space-y-1.5 items-start">
-              <Input id="password" placeholder="Confirm Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+              <Input id="password" placeholder="Confirm Password" type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
             </div>
             <Button>Create Account</Button>
           </div>
